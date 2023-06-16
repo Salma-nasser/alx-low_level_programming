abf2bd7fe8019dbb2da519767d122796a9f01709
Salma nasser <salma.naser1020@gmail.com>
2023-06-16 13:21:49 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 successfully
+ */
+int main(void)
+{
+        char x;
+        for (x = 'a';x < 'z';x++)
+                putchar(x);
+	putchar('\n');
+        return (0);
+}
