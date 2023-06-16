b300bbb490e1a223ab886b6e8032b87f2f3e8b0d
Salma nasser <salma.naser1020@gmail.com>
2023-06-16 14:36:21 -0700

    5and6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main- entry point
+*Print numbers of base 10
+*Return: 0 (sucess)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
