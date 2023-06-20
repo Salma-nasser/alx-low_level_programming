19f96d6c8a06fbf5e1a2e5c4a69e51e088a9d181
Salma nasser <salma.naser1020@gmail.com>
2023-06-20 13:02:54 -0700

    print10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * print the alphabet 10 times
+ *
+ * Return: 0 sucess
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char ch;
+
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar("\n");
+	}
+}
