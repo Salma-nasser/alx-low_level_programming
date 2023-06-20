6c6df19e011b89354f6d8ec1d9568a3075e6f802
Salma nasser <salma.naser1020@gmail.com>
2023-06-20 08:55:27 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,37 @@
+#include <stdio.h> 
+
+/**main – entry point  
+
+* 
+
+*print –putchar  
+
+* 
+
+* Return: 0 sucess 
+
+*/ 
+
+int main(void) 
+
+{ 
+
+	putchar(95); 
+
+	putchar('p'); 
+
+	putchar('u'); 
+
+	putchar('t'); 
+
+	putchar('c'); 
+
+	putchar('h'); 
+
+	putchar('a'); 
+
+	putchar('r'); 
+
+	putchar('\n'); 
+
+} 
