fbf0865f3744a830c519b8d869fd1bccb10965a6
Salma nasser <salma.naser1020@gmail.com>
2023-06-22 16:05:57 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isupper - entry point
+ *
+ *check if the input is an upper case or not
+ *
+ * Return: 1 if uppercase 0 if lowercase
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
