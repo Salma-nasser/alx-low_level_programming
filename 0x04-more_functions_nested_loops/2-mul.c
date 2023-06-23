8e7af9d45e6e7d29c5e9b25ed5fdf576af4eee01
Salma nasser <salma.naser1020@gmail.com>
2023-06-22 17:15:29 -0700

    2 tasks together

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
- *mul - entry point
- *@a: first int
- *@b: second int
- *
- *Return : a*b
+ * mul - multiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplicattion
  */
+
 int mul(int a, int b)
 {
 	return (a * b);
