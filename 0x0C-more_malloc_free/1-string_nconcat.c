175bc8190fbbbce09fdc5831248a59b1af87e764
Salma nasser <salma.naser1020@gmail.com>
2023-07-13 18:20:36 -0700

    concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,6 +16,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len1++;
 	while (s2[len2])
 		len2++;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	if (n < len2)
 		new = malloc(sizeof(char) * (len1 + n + 1));
 	else
