72929767c77f812a68224659a964e64e8b4b7ceb
Salma nasser <salma.naser1020@gmail.com>
2023-07-17 15:46:02 -0700

    come on

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -1,14 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 #include "dog.h"
-/**
-* main - check the code
-* Return: Always 0
-*/
-
 int main(void)
 {
 	struct dog my_dog;
-
 	my_dog.name = "Poppy";
 	my_dog.age = 3.5;
 	my_dog.owner = "Bob";
