61d2866b20a1a359eb1bf9f2412cbdded4db849f
Salma nasser <salma.naser1020@gmail.com>
2023-07-17 16:41:35 -0700

    4-dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int len1 = 0, len2 = 0, i, j;
+
+	while (name[len1] != '\0')
+		len1++;
+	while (owner[len2] != '\0')
+		len2++;
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->age = age;
+	for (i = 0; i < len1; i++)
+	{
+		dog->name[i] = name[i];
+	}
+	for (j = 0; j < len2; j++)
+	{
+		dog->owner[j] = owner[j];
+	}
+	return (dog);
+}
