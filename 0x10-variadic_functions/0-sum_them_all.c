926e6f76b0194356893de8e685862da14bdd4685
Salma nasser <salma.naser1020@gmail.com>
2023-07-20 17:31:01 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,7 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
-		sum + = va_arg(arg, int);
+		sum+= va_arg(arg, int);
 	}
 	va_end(arg);
 	return (sum);
