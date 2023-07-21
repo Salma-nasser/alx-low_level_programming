480af9493f61d8cee0c0416d06db6d7551759d26
Salma nasser <salma.naser1020@gmail.com>
2023-07-20 17:42:38 -0700

    numbers and sums

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - print numbers
+ * @separator: char seperator betwwen each number
+ * @n: constant unsigned int
+ * @...:unkown number of variable parameters
+ * Return: Always 0 success
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int i;
+
+	va_start(num, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(num, int));
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(num);
+}
