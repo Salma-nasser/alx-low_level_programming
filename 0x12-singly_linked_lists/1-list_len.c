ec84830c3f270eef558c1225e022398d3f90121b
Salma nasser <salma.naser1020@gmail.com>
2023-07-27 17:21:10 -0700

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - prints length of list
+ * @h: head of linked list
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned long i = 0;
+	
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
