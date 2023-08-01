443cce2d5617cc51e60f8664e9fb41a29c09a0fa
Salma nasser <salma.naser1020@gmail.com>
2023-08-01 16:44:38 -0700

    free list 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,15 +7,15 @@
  */
 void free_listint2(listint_t **head)
 {
-        listint_t *tmp;
+	listint_t *tmp;
 
 	if (head == NULL)
 		return;
-        while (*head)
-        {
-                tmp = (*head)->next;
-                free(*head);
-                (*head) = tmp;
-        }
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		(*head) = tmp;
+	}
 	*head = NULL;
 }
