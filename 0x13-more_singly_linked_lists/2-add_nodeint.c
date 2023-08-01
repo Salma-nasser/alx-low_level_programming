dfb1da5d273e331c0bacf7277a10f0fb0dab6d0e
Salma nasser <salma.naser1020@gmail.com>
2023-08-01 16:06:22 -0700

    nice

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_nodeint - add node
+ * @head: pointer to pointer to head
+ * @n: node contents
+ * Return: pointer to head
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *node;
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	node->n = n;
+	node->next = (*head);
+	(*head) = node;
+	return (*head);
+}
