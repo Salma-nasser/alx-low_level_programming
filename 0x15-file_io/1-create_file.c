c06f702640ae34e21688d115b72cd517943fb442
Salma nasser <salma.naser1020@gmail.com>
2023-08-07 14:02:20 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of file to be created
+ * @text_content: text to be written inside file
+ * Return: 1 on sucess
+ * -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fid, wr, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	while (text_content[len] != '\0')
+		len++;
+	fid = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(fid, text_content, len);
+	if (wr == -1 || fid == -1)
+		return (-1);
+	close(fid);
+	return (1);
+}
