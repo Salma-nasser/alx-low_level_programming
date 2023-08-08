43f45c607c2c10c5a6ffb715f288575e051f4935
Salma nasser <salma.naser1020@gmail.com>
2023-08-07 17:26:53 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,13 +12,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	while (text_content[len] != '\0')
-		len++;
-	if (text_content == NULL)
-		len = 0;
 	fid = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fid == -1)
+		return (-1);
+	if (text_content)
+		while (text_content[len])
+			len++;
 	wr = write(fid, text_content, len);
-	if (wr == -1 || fid == -1)
+	if (wr == -1)
 		return (-1);
 	close(fid);
 	return (1);
