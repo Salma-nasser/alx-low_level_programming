6c9a078f0de69d3cfd4edb53d52afef2fcfee6bf
Salma Nasser <salma.naser1020@gmail.com>
2024-01-29 15:58:25 -0800

    salma

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - adds two int
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtracts two int
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplies two int
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divides two int
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - returns the remainder of two int
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
