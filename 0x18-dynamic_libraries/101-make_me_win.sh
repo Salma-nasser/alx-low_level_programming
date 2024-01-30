daffd1abfec93dd69604725cb24d1c512e057917
Salma Nasser <salma.naser1020@gmail.com>
2024-01-29 17:15:44 -0800

    salmaa

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P .. https://raw.githubusercontent.com/Salma-nasser/alx-low_level_programming/master/0x18-dynamic_libraries/hackinglib.so
-export LD_PRELOAD="$PWD/../hackinglib.so"
+export LD_PRELOAD=/$PWD/../hackinglib.so
